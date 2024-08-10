87de48ed231e0f986ec90453475e289cf2a743a1
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2024-08-10 14:51:38 +0500

    wecinema

--- backend/src/firebaseAdmin.js
+++ backend/src/firebaseAdmin.js
@@ -1,14 +1,9 @@
-require('dotenv').config(); // Load environment variables from .env file
-console.log(process.env.FIREBASE_SERVICE_ACCOUNT); // Check if this outputs the correct JSON string
-
 const admin = require('firebase-admin');
-const fs = require('fs');
-
-// Parse the service account JSON string from the environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = require('../serviceAccount.json'); // Ensure this path is correct
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://your-database-name.firebaseio.com"
 });
 
 module.exports = admin;
